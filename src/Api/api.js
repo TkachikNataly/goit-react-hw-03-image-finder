ba5a8564b0c8426d1d3f6d91cfdd51044eef642c
TkachikNataly <tkachik.nataly@gmail.com>
2022-05-16 02:18:24 +0200

    push 1

---
+++ src/Api/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+const fetchImages = async (query, currentPage) => {
+    const { data } = await axios.get(
+        `/?q=${query}&page=${currentPage}&key=3970e02ca371c5f8341f48ac2&q&image_type=photo&orientation=horizontal&per_page=12`
+    );
+
+    return data;
+};
+
+export default fetchImages;
